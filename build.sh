b10a02c1269ae616829eccf57b154a470eb5e0e2
Kenneth Baillie <github@baillielab.net>
2024-10-10 20:00:25 +0100

    add token and build instructions

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git config --file=.gitmodules submodule.shared-files.url "https://$GITHUB_TOKEN@github.com/learningmodeloflife/shared-files.git"
+
+git submodule sync
+git submodule update --init --remote --merge
+
+jekyll build
