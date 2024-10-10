0057d418748866a1924d862aa8974dc13b17a04e
kennethbaillie <kennethbaillie@gmail.com>
2024-10-10 15:43:16 +0100

    build and netlify

--- build.sh
+++ build.sh
@@ -1,2 +1,11 @@
-JEKYLL_ENV=production bundle exec jekyll clean
-JEKYLL_ENV=production bundle exec jekyll build
+#!/bin/bash
+
+# Fail the build if anything goes wrong
+set -e
+
+echo "Initializing and updating Git submodules..."
+git submodule init
+git submodule update --recursive
+
+jekyll build 
+
