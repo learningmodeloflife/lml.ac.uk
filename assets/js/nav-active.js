4dd4043cf5b5f25ddac0977bdf5806a94f66b7cb
Kenneth Baillie <github@baillielab.net>
2024-10-18 20:13:08 +0100

    strip trailing /

--- assets/js/nav-active.js
+++ assets/js/nav-active.js
@@ -11,11 +11,22 @@ function getFilePath(url) {
     }
 }
 
+function stripTrailing(string) {
+    if (string.length > 1 && string.endsWith("/")) {
+        return string.slice(0, -1);
+    }
+    else {
+        return string
+    }
+
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     var currentPath = window.location.pathname;
     var navLinks = document.querySelectorAll('.nav-link');
     navLinks.forEach(function(link) {
-      linkpath = getFilePath(link.getAttribute('href'))
+      linkpath = stripTrailing(getFilePath(link.getAttribute('href')))
+      currentPath = stripTrailing(currentPath)
       console.log(linkpath + " | " + currentPath)
       if (linkpath === currentPath) {
         link.classList.add('active');
