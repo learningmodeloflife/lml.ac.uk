b2a66593a0b4f0b0a548c1b926f67ddffaef2468
Kenneth Baillie <github@baillielab.net>
2024-10-18 19:47:00 +0100

    cache add debugging

--- assets/js/nav-active.js
+++ assets/js/nav-active.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     var currentPath = window.location.pathname;
     var navLinks = document.querySelectorAll('.nav-link');
+    console.log("nav js called")
     navLinks.forEach(function(link) {
+      console.log(link)
       if (link.getAttribute('href') === currentPath) {
         link.classList.add('active');
+        console.log("--> active")
       }
     });
   });
