6399569c5974b6693d0ac3de8486223de2a78be8
Kenneth Baillie <github@baillielab.net>
2024-10-20 17:32:07 +0100

    fuck it

--- build.sh
+++ build.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-git config --file=.gitmodules submodule.shared-files.url "https://$GITHUB_TOKEN@github.com/learningmodeloflife/shared-files.git"
-git submodule sync 
-git submodule update --init --remote --merge
-
 jekyll build
