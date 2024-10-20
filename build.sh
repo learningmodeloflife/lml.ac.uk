9a54963f1baa09352538d9a047a438bc6f2394cd
Kenneth Baillie <github@baillielab.net>
2024-10-20 17:26:02 +0100

    add submodule again; uncomment build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#git config --file=.gitmodules submodule.shared-files.url "https://$GITHUB_TOKEN@github.com/learningmodeloflife/shared-files.git"
-#git submodule sync 
-#git submodule update --init --remote --merge
+git config --file=.gitmodules submodule.shared-files.url "https://$GITHUB_TOKEN@github.com/learningmodeloflife/shared-files.git"
+git submodule sync 
+git submodule update --init --remote --merge
 
 jekyll build
